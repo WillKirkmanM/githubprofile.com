cda25806bc0bc9f97288e2d4e3aa496504d7c154
WillKirkmanM <98240335+WillKirkmanM@users.noreply.github.com>
2024-01-19 12:16:23 +0000

    Fix(env.js): :) Thank you

--- src/env.js
+++ src/env.js
@@ -52,7 +52,7 @@ export const env = createEnv({
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GITHUb_CLIENT_SECRET,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
